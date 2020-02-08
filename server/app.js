50a20f302fd16060fa534f14867cdc06ebe6a2ed
Dan Mihai Vidican <vidican.dan@gmail.com>
2020-02-08 19:14:55 +0200

    new: app.js set up

---
+++ server/app.js
@@ -0,0 +1,24 @@
+require('rootpath')();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const jwt = require('./utils/jwt.js');
+const errorHandler = require('./utils/errorHandler.js');
+const healthCheck = require('./utils/healthCheck.js');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use("/", healthCheck)
+app.use(jwt());
+
+app.use('/users', require('./user_management/user_controller.js'));
+
+app.use(errorHandler);
+
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const server = app.listen(port, function () {
+    console.log('Server listening on port ' + port);
+});
